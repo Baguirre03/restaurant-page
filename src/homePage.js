6158da565e280b597008e0ac52d8841cc74e8583
Ben Aguirre <ben.aguirre03@gmail.com>
2023-03-11 15:19:59 -0600

    Reorganize code

--- src/homePage.js
+++ src/homePage.js
@@ -2,7 +2,17 @@ import { createMainContainer, createChildren, createButton } from "./basicFuncti
 
 //For functions (text, parentClass, className)
 const renderHomePage = () => {
-    createMainContainer('textawadw', 'class', 'class');
+    createMainContainer('', 'body', 'main-container');
+
+    //header
+    createChildren('', '.main-container', 'header')
+    createChildren('Coffee shop', '.header')
+
+    createChildren('', '.header', 'button-holder')
+
+    createButton('button1', '.button-holder', 'buttons')
+    createButton('button2', '.button-holder', 'buttons')
+    createButton('button3', '.button-holder', 'buttons')
 }
 
 export {renderHomePage}
