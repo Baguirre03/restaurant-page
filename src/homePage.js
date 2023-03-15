09528c921e22155689265b4fe8f5ceb0a5b12f5d
Ben Aguirre <ben.aguirre03@gmail.com>
2023-03-15 18:48:30 -0500

    Add background to homepage

--- src/homePage.js
+++ src/homePage.js
@@ -7,8 +7,11 @@ import {
 // syntax: createBlank(text, parentClass, className)
 const renderHomePage = () => {
   createMainContainer("", "main-container");
+  const backgroundContainer = document.querySelector(".main-container");
+  backgroundContainer.classList.add("home-container");
 
-  createChildren("", ".main-container", "about-container");
+  createChildren("", ".main-container", "second-main");
+  createChildren("", ".second-main", "about-container");
   createChildren("About", ".about-container", "about-header");
   createChildren(
     "Welcome to our coffee shop, where we believe that every cup of coffee should be an experience to remember. Located in the heart of the city, our coffee shop is the perfect spot to unwind and relax, catch up with friends or get some work done. We pride ourselves on providing the highest quality coffee, sourced from the finest roasters around the world.Located in the heart of the city, our coffee shop is the perfect spot to unwind and relax, catch up with friends or get some work done. We pride ourselves on providing the highest quality coffee, sourced from the finest roasters around the world.",
@@ -16,7 +19,7 @@ const renderHomePage = () => {
     "about-text"
   );
 
-  createChildren("", ".main-container", "hours-container");
+  createChildren("", ".second-main", "hours-container");
   createChildren("Hours", ".hours-container", "hours-header");
   createChildren(
     "Our coffee shop is open 7 days a week for your convenience. Our hours of operation are as follows: Monday to Friday: 7:00am - 7:00pm Saturday: 8:00am - 6:00pm Sunday: 9:00am - 5:00pm We understand that everyone has different schedules, so we aim to offer flexible hours that can accommodate a variety of lifestyles. Whether you need your morning caffeine fix or a cozy place to unwind in the evening, our doors are always open.",
@@ -24,7 +27,7 @@ const renderHomePage = () => {
     "hours-text"
   );
 
-  createChildren("", ".main-container", "location-container");
+  createChildren("", ".second-main", "location-container");
   createChildren("Location", ".location-container", "location-header");
   createChildren(
     "6000 West drive, Dallas, TX",
