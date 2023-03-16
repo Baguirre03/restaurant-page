27c8188ad7ce529059d30134f0f5edbfb25b8931
Ben Aguirre <ben.aguirre03@gmail.com>
2023-03-16 18:33:45 -0500

    Add basic form styling

--- src/contact.js
+++ src/contact.js
@@ -4,14 +4,19 @@ import {
   createInput,
   createForm,
   createLabel,
+  createImg,
 } from "./basicFunctions";
 
 //syntax: (Text, parentClass, className)
 //input: (id, parentClass, LabelText)
+//Image: (imageSource, parentClass, className))
 const renderContact = () => {
   createMainContainer("", "main-container");
+  const backgroundContainer = document.querySelector(".main-container");
+  backgroundContainer.classList.add("contact-container");
 
   createChildren("", ".main-container", "contact-holder");
+  // createImg("/src/imgs/jason-leem.jpg", ".main-container", "side-image");
   createChildren("Contact Ben's Coffee!", ".contact-holder", "contact-header");
   createChildren(
     "Lets get this conversation started. Tell us a bit about  yourself and we`ll get in touch as soon as we can.",
