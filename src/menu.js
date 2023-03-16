c1793713443e7334fe3963bd72d6dd95f1ca5747
Ben Aguirre <ben.aguirre03@gmail.com>
2023-03-16 15:43:09 -0500

    Safety commit

--- src/menu.js
+++ src/menu.js
@@ -13,33 +13,38 @@ const createMenu = () => {
     "Espresso",
     "A strong and concentrated shot of coffee made by forcing hot water through finely ground coffee beans. It has a thick crema on top and a rich flavor.",
     ".menu-holder",
-    "/src/imgs/espresso.jpg"
+    "/src/imgs/espresso2.png"
   );
 
   createFoodItem(
     "Cappuccino",
     "A classic Italian coffee drink made with espresso, steamed milk, and a layer of frothed milk on top. It has a creamy texture and a balanced flavor.",
-    ".menu-holder"
+    ".menu-holder",
+    "/src/imgs/cap.png"
   );
   createFoodItem(
     "Americano",
     "A simple coffee drink made by adding hot water to a shot of espresso. It has a milder flavor than espresso and a higher volume.",
-    ".menu-holder"
+    ".menu-holder",
+    "/src/imgs/americano.png"
   );
   createFoodItem(
     "Latte",
     "A popular coffee drink made with espresso and steamed milk, typically with a small amount of foam on top. It has a smooth and creamy texture with a mild espresso flavor.",
-    ".menu-holder"
+    ".menu-holder",
+    "/src/imgs/latte.png"
   );
   createFoodItem(
     "Macchiato",
     "A small shot of espresso with a dollop of steamed milk on top. It has a bolder flavor than a latte and a lighter texture than a cappuccino.",
-    ".menu-holder"
+    ".menu-holder",
+    "/src/imgs/machiatto.png"
   );
   createFoodItem(
     "Mocha",
     "A coffee drink made with espresso, steamed milk, chocolate syrup or powder, and whipped cream on top. It has a rich and indulgent flavor with a hint of chocolate.",
-    ".menu-holder"
+    ".menu-holder",
+    "/src/imgs/mocha.png"
   );
 };
 
